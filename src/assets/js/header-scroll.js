f3958121330555f6f324909f888f8c8043811802
Gustavo Torres <vforce19@LIVE.COM>
2022-02-25 19:35:48 -0300

    Fix: Change header menu active when scrolled page

--- src/assets/js/header-scroll.js
+++ src/assets/js/header-scroll.js
@@ -1,28 +1,26 @@
-window.addEventListener("scroll", (e) =>{
+window.addEventListener("scroll", (e) => {
     const scrollValue = this.scrollY;
     const headerElement = document.getElementById('header');
 
-    let text = 'none';   
-    if (scrollValue > 10)        
+    let text = 'none';
+    if (scrollValue > 10)
         text = 'flex';
-    
+
     if (headerElement.style.display != text)
         headerElement.style.display = text;
 
     const sections = document.querySelectorAll("section");
-    if (sections.length > 0)
-    {        
+    if (sections.length > 0) {
         sections.forEach(section => {
-            if (isInViewport(section))        
-            {
-                const className = section.className.split(" ")[0];        
-                let currentActive = document.getElementsByClassName("menu-active")[0];              
-
-                if (currentActive.classList.contains("menu-active"))       
+            const rect = section.getBoundingClientRect();
+            if (rect.top <= 0) {
+                const className = section.className.split(" ")[0];
+                let currentActive = document.getElementsByClassName("menu-active")[0];
+                if (currentActive.classList.contains("menu-active"))
                     currentActive.classList.remove("menu-active");
-                    
+
                 currentActive = document.getElementById(className);
-                
+
                 if (!currentActive.classList.contains(className))
                     currentActive.classList.add("menu-active");
             }
@@ -30,14 +28,12 @@ window.addEventListener("scroll", (e) =>{
     }
 })
 
-function isInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+// function isInViewport(el) {
+//     const rect = el.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight));
 
-    );
-}
+// };
+
 
