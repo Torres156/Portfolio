3dca2f92d95942ce5b72324071b4efdeae4e4c47
Gustavo Torres <vforce19@LIVE.COM>
2022-02-25 15:53:24 -0300

    Initial commit

---
+++ src/assets/js/header-scroll.js
@@ -0,0 +1,43 @@
+window.addEventListener("scroll", (e) =>{
+    const scrollValue = this.scrollY;
+    const headerElement = document.getElementById('header');
+
+    let text = 'none';   
+    if (scrollValue > 10)        
+        text = 'flex';
+    
+    if (headerElement.style.display != text)
+        headerElement.style.display = text;
+
+    const sections = document.querySelectorAll("section");
+    if (sections.length > 0)
+    {        
+        sections.forEach(section => {
+            if (isInViewport(section))        
+            {
+                const className = section.className.split(" ")[0];        
+                let currentActive = document.getElementsByClassName("menu-active")[0];              
+
+                if (currentActive.classList.contains("menu-active"))       
+                    currentActive.classList.remove("menu-active");
+                    
+                currentActive = document.getElementById(className);
+                
+                if (!currentActive.classList.contains(className))
+                    currentActive.classList.add("menu-active");
+            }
+        });
+    }
+})
+
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+    );
+}
+
